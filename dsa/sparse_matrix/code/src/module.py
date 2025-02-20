1d91d72d268c40f29d30d7ee393012e23d379146
kambaikariuki <kariukikamba@gmail.com>
2025-02-20 12:17:03 +0300

    added module with functions

---
+++ dsa/sparse_matrix/code/src/module.py
@@ -0,0 +1,78 @@
+def read(file):
+    with open(file, "r") as f:
+        """Read matrices from a line"""
+
+        # Read the first two lines
+        rows = f.readline().strip()
+        columns = f.readline().strip()
+        
+        # Instantiate a list, and save the rest of the line in the file into a list
+        lines = []
+        for line in f:
+            lines.append().strip()
+
+        return lines, rows, columns
+        
+    #     i = 0
+    #     matrices = []
+    #     for line in f:
+    #         i += 1
+    #         if i > 2:
+    #             matrices.append(line)
+    # return matrices    
+
+
+
+
+def add(*matrix):
+    """Addition of sparse matrices, matrices passed as args"""
+    result = {}
+
+    for i, j, value in matrix:
+        result[i, j] = result.get((i,j), 0) + value
+
+    # Format the results
+    result = [(i, j, v) for (i, j), v in result.items() if v != 0]
+
+    return result
+
+def subtract(*matrix):
+    """Subtraction of matrices, matrices passed as args"""
+    result = {}
+    # Set first matrice into the result
+    first = matrix[0]
+    result = {first[:2]:first[2]}
+    # Subtract the rest by going through the rest 
+    for i, j, value in matrix[1:]:
+        result[i, j] = result.get((i,j), 0) - value
+
+
+    # Format the results
+    result = [(i, j, v) for (i, j), v in result.items() if v != 0]
+    return result
+
+
+def multiplication(*matrix):
+    pass
+
+def getElement(currRow, currCol)
+    """Reads the lines in a file and gets the non zero element in the row and column"""
+    read(file)
+    for line in lines:
+        if currRow, currCol:
+            return line
+        else:
+            return 0
+
+def setElement(currRow, currCol, value)
+    read(file)
+    for line in lines:
+        if not currRow, currCol:
+            lines.append(({currRow}, {currCol}, {value}))
+        else:
+            line[2] = value
+    return lines
+
+
+print(read("dsa/sparse_matrix/sample_inputs/ easy_sample_01_2.txt"))
+# print(add((0, 23, 632), (0, 32, 5456)))
